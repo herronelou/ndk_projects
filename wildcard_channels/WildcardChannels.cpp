54472e26289e8323bfd041ada92a393d82f03411
Erwan Leroy <herronelou@gmail.com>
2022-10-10 22:47:17 -0700

    Implement the non-regex patterns

--- wildcard_channels/WildcardChannels.cpp
+++ wildcard_channels/WildcardChannels.cpp
@@ -5,7 +5,7 @@ static const char* const RCLASS = "WildcardChannels";
 
 static const char* const HELP = "Removes/keeps color channels from the image based on wildcard expressions. ";
 
-/* Remove channels from the input. This is really simple to impelement
+/* Remove channels from the input. This is really simple to implement
    as it does nothing except change the info so the channels are not
    there.
  */
@@ -36,17 +36,38 @@ public:
     const char* node_help() const override { return HELP; }
     OpHints opHints() const override;
     static Iop::Description d;
+private:
+    const std::regex escape{ R"([.^$()\[\]{}+\\])" };  // Any of: . ^ $ ( ) [ ] { } + and backslash
 };
 
 void WildcardChannels::_validate(bool for_real)
 {
+    // Remove all spaces from pattern
+    size_t pos = pattern.find(' ');
+    while (pos != std::string::npos)
+    {
+        pattern.erase(pos, 1);
+        pos = pattern.find(' ');
+    }
+
+    copy_info();
+
+    // Skip on empty patterns
     if (pattern.empty())
     {
+        std::cout << "Early Exit" << std::endl;
         set_out_channels(Mask_None); // Tell Nuke we didn't touch anything.
         return;
     }
 
-    copy_info();
+    if (!regex_mode) {
+        std::cout << "Wildcard mode"<< std::endl;
+        // Convert the Wildcard format to a Regex
+        pattern = std::regex_replace(pattern, escape, R"(\$&)");
+        pattern = std::regex_replace(pattern, std::regex(R"(\*)"), R"(.*)");  // * means any number of any characters
+        pattern = std::regex_replace(pattern, std::regex(R"(\?)"), R"(.)");  // ? means one instance of any character
+        pattern = std::regex_replace(pattern, std::regex(R"(#)"), R"(\d)");  // # means one instance of a digit
+    }
 
     std::cout << "pattern is: " << pattern << std::endl;
 
@@ -56,7 +77,7 @@ void WildcardChannels::_validate(bool for_real)
     // Compile Regex
     std::regex regex_pattern;
     try {
-        regex_pattern = std::regex(pattern);
+        regex_pattern = std::regex(pattern, std::regex::icase);
     }
     catch (std::regex_error& e) {
         error(e.what());
@@ -65,7 +86,7 @@ void WildcardChannels::_validate(bool for_real)
     
     // Compare all channels
     for (Channel channel = current_chans.first(); channel; channel = current_chans.next(channel)) {
-        std::cout << getName(channel) << std::endl;
+        //std::cout << getName(channel) << std::endl;
         if (std::regex_match(getName(channel), regex_pattern)) {
             // std::cout << "Match!!!" << std::endl;
             matching_chans += channel;
@@ -78,14 +99,14 @@ void WildcardChannels::_validate(bool for_real)
             matching_chans += channels_to_keep;
         }
         info_.channels() &= matching_chans;
-        set_out_channels(info_.channels()); //?
+        set_out_channels(info_.channels());  // Should this be inverted to indicate the channels that were removed?
     }
     else {  // Remove
         if (channels_to_keep) {
             matching_chans -= channels_to_keep;
         }
         info_.turn_off(matching_chans);
-        set_out_channels(matching_chans); // Should this be inverted to indicate the channels that were removed?
+        set_out_channels(matching_chans);
     }
 }
 
@@ -101,11 +122,16 @@ void WildcardChannels::knobs(Knob_Callback f)
     // Pattern knob to type the regex or pattern
     String_knob(f, &pattern, "pattern"); 
     Tooltip(f, "Pattern to use for choosing which channels to keep or remove.\n"
-        "In normal mode, use \"?\" to match any character, \"#\" to match any digit character and \"*\" to match multiple characters.");
+        "In normal mode, use \"?\" to match any character, \"#\" to match any digit character and \"*\" to match multiple characters.\n"
+        "In Regex Mode, all symbols supported by C++ ECMAScript regexes are supported.\n"
+        "Pattern is case-insensitive. Multiple patterns can be joined with a \"|\" character.");
     Bool_knob(f, &regex_mode, "regex_mode", "Use Regex");
+    Tooltip(f, "Use Regex syntax.\nIf not checked, simple wildcard syntax will be used.");
     // Add a dropdown to bypass certain channels 
     Divider(f);
     Input_ChannelMask_knob(f, &channels_to_keep, 0, "channels_to_keep", "always keep");
+    Tooltip(f, "Choose channels to always keep, even if the wildcard would have normally removed them.\n"
+        "For example, you may want to always keep RGBA.");
 }
 
 OpHints WildcardChannels::opHints() const
